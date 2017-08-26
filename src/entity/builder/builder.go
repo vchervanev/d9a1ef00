ba06bea7ef9adfd1b29c641c73ff69ce8fc875cb
vladmir.chervanev <vladimir.chervanev@envato.com>
2017-08-26 15:28:55 +1000

    Allocate memory for entities

--- src/entity/builder/builder.go
+++ src/entity/builder/builder.go
@@ -2,9 +2,19 @@ package builder
 
 import "../../entity"
 
+func dup(src []byte) (result []byte) {
+	result = make([]byte, len(src))
+	copy(result, src)
+	return
+}
+
 func Build(definition *entity.Definition, names, values [][]byte) (result *entity.Record) {
-	record := entity.Record{Definition: definition, Data: values}
-	// TODO check values order!
+	data := make([][]byte, len(values))
+	count := len(names)
+	for i := 0; i < count; i++ {
+		data[i] = dup(values[i])
+	}
+	record := entity.Record{Definition: definition, Data: data}
 	return &record
 }
 
